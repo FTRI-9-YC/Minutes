278c0013c9e5d7bd2f2b3a5bc575fca95c58931e
ychang49265 <yc1135@nyu.edu>
2022-06-11 16:42:41 -0400

    files added

---
+++ client/components/NotePage/SideBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddNoteSection from './AddNoteSection';
+import NotePrevAccordian from './NotePrevAccordian';
+
+export default function SideBar (){
+    return (
+        <div>
+            <AddNoteSection />
+            <NotePrevAccordian />
+        </div>
+    )
+}
---
+++ client/components/NotePage/VideoSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NotesSummary from './NotesSummary';
+import VideoDisplayBox from './VideoDisplayBox';
+
+export default function VideoSection (){
+    return (
+        <section>
+            <NotesSummary />
+            <VideoDisplayBox />
+        </section>
+    )
+}
