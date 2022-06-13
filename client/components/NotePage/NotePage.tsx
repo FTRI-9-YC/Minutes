09307e3f554c69b05f5349dc1a0d4bc6ee3004bf
ychang49265 <yc1135@nyu.edu>
2022-06-13 13:42:52 -0400

    frontend updated

--- client/components/NotePage/NotePage.tsx
+++ client/components/NotePage/NotePage.tsx
@@ -1,50 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import VideoSection from './VideoSection';
-import SideBar from './SideBar';
-import NavBar from '../Global/NavBar';
-import YouTube, { YouTubePlayer, YouTubeEvent, YouTubeProps } from 'react-youtube';
-import getYouTubeID from 'get-youtube-id';
+import React, { useEffect, useState } from "react";
+import VideoSection from "./VideoSection";
+import SideBar from "./SideBar";
+import NavBar from "../Global/NavBar";
+import YouTube, {
+  YouTubePlayer,
+  YouTubeEvent,
+  YouTubeProps,
+} from "react-youtube";
+import getYouTubeID from "get-youtube-id";
 
-export default function NotePage (){
-    // State for testing - can delete if needed
-    const [videoObject, setVideoObject] = useState<YouTubePlayer>();
-    const [timeStamp, setTimeStamp] = useState();
-    const [url, setUrl] = useState();
-    const[id, setId] = useState('');
+export default function NotePage() {
+  // State for testing - can delete if needed
+  const [videoObject, setVideoObject] = useState<YouTubePlayer>();
+  const [time, setTime] = useState(0);
+  const [youtubeLink, setYoutubeLink] = useState("");
+  const [id, setId] = useState("");
+  const [content, setContent] = useState("");
+  useEffect(() => {
+    // fetch data from database
+  });
 
-    useEffect(() => {
-        // fetch data from database
-    })
+  // any time there is a change in state (press play/pause), current time is aquired
+  const onPlayerStateChange: YouTubeProps["onStateChange"] = (
+    e: YouTubeEvent<number>
+  ) => {
+    console.log(e);
+  };
 
-    // any time there is a change in state (press play/pause), current time is aquired
-    const onPlayerStateChange: YouTubeProps['onStateChange'] = (e: YouTubeEvent<number>) => {
-        console.log(e);
-        console.log(e.target.getCurrentTime());
+  // once video loads, function fires and video is automatically paused for user to press play
+  const onPlayerReady: YouTubeProps["onReady"] = (e) => {
+    // set target state to player obj (use for pause button)
+    setVideoObject(e.target);
+    e.target.pauseVideo();
+  };
+
+  const handleInputChange = (val: string) => {
+    setId(getYouTubeID(val));
+  };
+
+  // handles note button pause, sets time stamp in state
+  const handleNoteInput = (val: string) => {
+    if (!videoObject) return console.error("Target does not exist");
+    videoObject.pauseVideo();
+    if (time === 0) {
+      setTime(videoObject.getCurrentTime());
     }
+    setContent(val);
+  };
 
-    // once video loads, function fires and video is automatically paused for user to press play
-    const onPlayerReady: YouTubeProps['onReady'] = (e) => {
-        // set target state to player obj (use for pause button)
-        setVideoObject(e.target);
-        e.target.pauseVideo();
-    }
-
-    const handleInputChange = (val: string) => {
-        setId(getYouTubeID(val));
-    }
-
-    // handles note button pause, sets time stamp in state
-    const handleNoteButtonPause = () => {
-        if (!videoObject) return console.error('Target does not exist');
-        videoObject.pauseVideo();
-        setTimeStamp(videoObject.getCurrentTime());
-    }
-
-    return (
-        <div>
-            <NavBar />
-            <VideoSection onPlayerReady={onPlayerReady} onPlayerStateChange={onPlayerStateChange} handleInputChange={ handleInputChange } id={id} />
-            <SideBar handleNoteButtonPause={handleNoteButtonPause} />
-            </div>
-    )
+  return (
+    <div>
+      <NavBar />
+      <VideoSection
+        onPlayerReady={onPlayerReady}
+        onPlayerStateChange={onPlayerStateChange}
+        handleInputChange={handleInputChange}
+        id={id}
+      />
+      <SideBar
+        handleNoteInput={handleNoteInput}
+        youtubeLink={youtubeLink}
+        time={time}
+        content={content}
+      />
+    </div>
+  );
 }
