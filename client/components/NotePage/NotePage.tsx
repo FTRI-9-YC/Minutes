7fb65f0bdfa222f4c1d1b5df4e0639c05659fa1d
Jeff Pettis <jeffrey.pettis@gmail.com>
2022-06-12 17:17:50 -0700

    Added youtube iframe and url input box

--- client/components/NotePage/NotePage.tsx
+++ client/components/NotePage/NotePage.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import VideoSection from './VideoSection';
 import SideBar from './SideBar';
 import NavBar from '../Global/NavBar';
-
+import YouTube, { YouTubeEvent, YouTubeProps } from 'react-youtube';
 
 export default function NotePage (){
+    const [target, setTarget] = useState({});
+    // any time there is a change in state (press play/pause), current time is aquired
+    const onPlayerStateChange: YouTubeProps['onStateChange'] = (e: YouTubeEvent<number>) => {
+        console.log(e);
+        console.log(e.target.getCurrentTime());
+    }
+
+    // once video loads, function fires and video is automatically paused for user to press play
+    const onPlayerReady: YouTubeProps['onReady'] = (e) => {
+        // set target state to player obj (use for pause button)
+        setTarget(e.target);
+        // access to player in all event handlers via event.target
+        e.target.pauseVideo();
+    }
+
+    // const handleNoteButtonPause = (event) => {
+
+    // }
+
     return (
         <div>
             <NavBar />
-            <VideoSection />
+            <VideoSection onPlayerReady={onPlayerReady} onPlayerStateChange={onPlayerStateChange} />
             <SideBar />
             </div>
     )
--- client/components/NotePage/VideoSection.tsx
+++ client/components/NotePage/VideoSection.tsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import NotesSummary from './NotesSummary';
 import VideoDisplayBox from './VideoDisplayBox';
+import getYouTubeID from 'get-youtube-id';
 
-export default function VideoSection (){
+interface stateChangeProps {
+    onPlayerReady: (val: object) => void,
+    onPlayerStateChange: (val: object) => void
+}
+
+export default function VideoSection ({ onPlayerReady, onPlayerStateChange }: stateChangeProps){
+    // state for timestamp
+    const [timeStamp, setTimeStamp] = useState('');
+    const[id, setId] = useState('');
+    
+    const handleInputChange = (val: string) => {
+        setId(getYouTubeID(val));
+    }
+
     return (
         <section>
             <NotesSummary />
-            <VideoDisplayBox />
+            <VideoDisplayBox id={id} handleInputChange={handleInputChange} onPlayerReady={onPlayerReady} onPlayerStateChange={onPlayerStateChange} />
         </section>
     )
 }
